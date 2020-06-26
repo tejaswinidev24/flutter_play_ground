26e083fe2d72c775ba106bfb482c847a3262e7dd
Tejaswini <tejaswinidev24@gmail.com>
2020-06-26 03:03:27 -0700

    added

--- mealsapp/lib/categorymealsscreen.dart
+++ mealsapp/lib/categorymealsscreen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+//import './categoryitem.dart';
+
 
 class CategoryMeals extends StatelessWidget {
+  /*final String categoryid;
+  final String categorytitle;
+  
+
+  CategoryMeals(this.categoryid, this.categorytitle);*/
+
+
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map <String, String>;
+    final categoryTitle = routeArgs['title'];
     return Scaffold(
       appBar: AppBar(
-        title: Text('Reciepies'),
+        title: Text(categoryTitle),
       ),
           body: Container(
         child: Center(
